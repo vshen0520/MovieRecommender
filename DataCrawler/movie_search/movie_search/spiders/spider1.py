eff99879fb84f498348a21f313a596ae86260a47
vshen0520 <42666102+vshen0520@users.noreply.github.com>
2022-11-25 23:01:18 -0500

    Create a demo spider

---
+++ DataCrawler/movie_search/movie_search/spiders/spider1.py
@@ -0,0 +1,37 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    # unique identifier for the spider
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+    
+    # CODES BELOW: copy down the html code of the page
+    # def start_requests(self):
+    #     urls = [
+    #         'http://quotes.toscrape.com/page/1/',
+    #         'http://quotes.toscrape.com/page/2/',
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = 'quotes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+    #     self.log('Saved file %s' % filename)
+    
+    
+    # CALL IT BY: scrapy crawl quotes -o quotes.csv
+    # at the command line of the project folder
