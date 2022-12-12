7fbe2dcf135dbdd249fa9d7e63bcc2f851adb232
vshen0520 <42666102+vshen0520@users.noreply.github.com>
2022-12-12 14:00:26 -0500

    Welcome interface setting

--- UI/djangochat/djangochat/urls.py
+++ UI/djangochat/djangochat/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""djangochat URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("chat.urls"))
+    path("", include("chatting.urls"))
 ]
